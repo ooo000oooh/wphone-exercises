940a19a539b5818f1ee51cea887b0ba78fad4bcc
ooo000oooh <antti.palomaki2@outlook.com>
2015-01-27 23:26:09 +0200

    teeeeeeeee

---
+++ EkaTest/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EkaTest
+{
+    public sealed class Class1
+    {
+        //just a comment testing
+    }
+}
